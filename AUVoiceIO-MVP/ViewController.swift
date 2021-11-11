5cade8ef76e58b21d3730f58420b295a82affc2b
Chad Parker <cparker@rzsoftware.com>
2021-11-11 09:40:09 -0800

    Clean up initial

--- AUVoiceIO-MVP/ViewController.swift
+++ AUVoiceIO-MVP/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  AUVoiceIO-MVP
-//
-//  Created by Chad Parker on 11/10/21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
